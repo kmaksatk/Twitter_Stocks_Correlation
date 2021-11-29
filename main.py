48cc5833025e9f043f90633de5ba67059a95417d
kmaksatk <keneshanovmak@gmail.com>
2021-11-29 21:12:37 +0400

    Check

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ import correlation
 if __name__ == '__main__':
     # data_preprocessing.clean_stocks()
     tw_with_stopwords = data_preprocessing.clean_tweets_keep_stopwords()
-    print(tw_with_stopwords.shape)
